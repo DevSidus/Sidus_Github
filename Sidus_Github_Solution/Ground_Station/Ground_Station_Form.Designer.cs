3c2832cdfda254e85c72bdfe0c90048576ea33d9
Ahmet Agca <ahmet_agca@hotmail.com>
2016-11-06 13:45:01 +0300

    Second commit

--- Sidus_Github_Solution/Ground_Station/Ground_Station_Form.Designer.cs
+++ Sidus_Github_Solution/Ground_Station/Ground_Station_Form.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1314, 503);
+            this.ClientSize = new System.Drawing.Size(1267, 503);
             this.Name = "Ground_Station";
             this.Text = "Ground Station v0.1";
             this.ResumeLayout(false);
