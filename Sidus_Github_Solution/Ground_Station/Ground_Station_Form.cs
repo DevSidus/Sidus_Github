7a9c00d4fc47bf5500fbcb4ff6d0d900896428c3
Ahmet Agca <ahmet_agca@hotmail.com>
2016-11-06 12:46:01 +0300

    Initial

---
+++ Sidus_Github_Solution/Ground_Station/Ground_Station_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ground_Station
+{
+    public partial class Ground_Station : Form
+    {
+        public Ground_Station()
+        {
+            InitializeComponent();
+        }
+    }
+}
