0b152f58aaef9445d4c1bffd9ee9fc9248b887a3
Ahmet Agca <ahmet_agca@hotmail.com>
2016-11-10 20:59:23 +0300

    Drone Flight Controller C++ Project is added into solution

---
+++ Sidus_Github_Solution/Drone_Flight_Controller/Drone_Flight_Controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Drone_Flight_Controller.ino
+ Created:	11/10/2016 8:56:34 PM
+ Author:	SIDUS
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
