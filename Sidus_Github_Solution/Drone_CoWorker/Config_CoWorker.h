c0ae118b2b415d6c45c708bafcbfe9f4c071665f
Ahmet Agca <ahmet_agca@hotmail.com>
2016-12-04 12:25:38 +0300

    MPU6050 Integration Created

--- Sidus_Github_Solution/Drone_CoWorker/Config_CoWorker.h
+++ Sidus_Github_Solution/Drone_CoWorker/Config_CoWorker.h
@@ -13,7 +13,9 @@ This header file define all the configurable variables including constants, pin 
 
 //Pin Definitions
 #define PIN_LED				5
-#define PIN_MPU_POWER_ON	15
+#define PIN_MPU_POWER_ON	16
+#define PIN_MCU_SDA			12
+#define PIN_MCU_SCL			14
 
 
 
@@ -27,3 +29,29 @@ This header file define all the configurable variables including constants, pin 
 //Global Variable Declarations
 int test_task_counter = 0;
 
+
+// MPU control/status vars
+bool mpuStatus;
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+						// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+int16_t gg[3];         // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+unsigned long mpuProcessStartTime = 0;
+short mpuProcessTaskDuration = 0;
+
+
+double mpuPitchAngle, mpuRollAngle, mpuYawAngle;
