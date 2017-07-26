e2ba8f9beebc3605173191bec124534051693ccf
myanik <meminyanik@live.com>
2017-07-26 14:41:37 +0300

    Kalman filter algoritm is updated (cascaded Kalman implementation)

--- Sidus_Github_Solution/Drone_Flight_Controller_2/kalmanFilter.h
+++ Sidus_Github_Solution/Drone_Flight_Controller_2/kalmanFilter.h
@@ -8,12 +8,17 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "rtwtypes.h"
-#include "kalmanFilter_types.h"
 
 // Function Declarations
+
+// Kalman Filter for 3 Parameters Estimation with 2 Measurements
 extern void kalmanFilter(const double m_n1[3], const double P_n1[9], const
 	double y_n[2], const double F[9], const double Q[9], const double H[6], const
 	double R[4], double m_n[3], double P_n[9]);
+
+// Kalman Filter for 1 Parameter Estimation with 1 Measurement
+extern void kalmanFilterOneParameter(double m_n1, double P_n1, double y_n, double F, double
+	Q, double H, double R, double *m_n, double *P_n);
 
 #endif
 
