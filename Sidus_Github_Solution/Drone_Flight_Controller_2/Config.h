2f39af1c1f7326a8a3402361e17bf39853252561
myanik <meminyanik@live.com>
2017-08-18 21:30:22 -0500

    Ultrasonic sensor implementation was updated (LPF and burst control algorithm were added). Altitude PID Coefficients Updated. Altitude Kalman impelementation was updated. It also uses ultrasonic sensor data below some threshold. Position PID Bug is fixed.

--- Sidus_Github_Solution/Drone_Flight_Controller_2/Config.h
+++ Sidus_Github_Solution/Drone_Flight_Controller_2/Config.h
@@ -169,16 +169,16 @@ bool barometer_initial_measurement = true;
 #define		PID_ANGLE_YAW_OUTMAX		150
 
 
-#define		PID_POS_ALT_KP			80
+#define		PID_POS_ALT_KP			60 // May be tuned better
 #define		PID_POS_ALT_KI			0.0
-#define		PID_POS_ALT_KD			0.0
+#define		PID_POS_ALT_KD			0.0 // May be tuned better
 #define		PID_POS_ALT_OUTMIN		-250
 #define		PID_POS_ALT_OUTMAX		250
 
 
-#define		PID_VEL_ALT_KP			4.0
+#define		PID_VEL_ALT_KP			5.0 // May be tuned better
 #define		PID_VEL_ALT_KI			0.0
-#define		PID_VEL_ALT_KD			0.1 // May be increased
+#define		PID_VEL_ALT_KD			0.1 // May be tuned better
 #define		PID_VEL_ALT_OUTMIN		-250
 #define		PID_VEL_ALT_OUTMAX		250
 
@@ -264,6 +264,7 @@ double barometerPress;
 bool baroReady = false;
 
 double ultrasonicDistance = 0.0;
+double ultrasonicDistanceFiltered = 0.0;
 
 float compassHdg;
 
@@ -455,6 +456,7 @@ struct3Dvector velPIDoutputLowpassBuffer;
 struct3Dvector accelDiffBuffer;
 struct3Dvector accelCmdDiffBuffer;
 
+vector<double> ultrasonicDistanceLowpassBuffer;
 
 double deltaTimeGyroDiff; // will be used at Exact Filtering
 double deltaTimeRateCmdDiff; // will be used at Exact Filtering
