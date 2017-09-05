465371b42e407f61b3b956a3810ed1c4c37706ff
myanik <meminyanik@live.com>
2017-09-05 18:50:44 -0500

    Kalman implementation for Compass Heading Estimation based on Measured Compass Heading and Yaw Rate

--- Sidus_Github_Solution/Drone_Flight_Controller_2/kalmanFilter.h
+++ Sidus_Github_Solution/Drone_Flight_Controller_2/kalmanFilter.h
@@ -20,8 +20,12 @@ extern void kalmanFilter(const double m_n1[3], const double P_n1[9], const
 extern void kalmanFilterOneParameter(double m_n1, double P_n1, double y_n, double F, double
 	Q, double H, double R, double *m_n, double *P_n);
 
-#endif
+// Kalman Filter for 2 Parameter Estimation with 2 Measurement
+extern void kalmanFilterAngleEstimation(const double m_n1[2], const double P_n1[4], const
+	double y_n[2], const double F[4], const double Q[4], const double H[4], const
+	double R[4], double m_n[2], double P_n[4]);
 
+#endif
 //
 // File trailer for kalmanFilter.h
 //
